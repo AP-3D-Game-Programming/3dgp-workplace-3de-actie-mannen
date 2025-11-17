e390519102ac248096fa59379175ef08dccbac12
witmilk <alexander.winkel1999@gmail.com>
2025-11-17 20:08:52 +0100

    removed CheckCrouch from Update, runs smoother now.

--- DeNachtwachtUnity/Assets/Scripts/Patrol.cs
+++ DeNachtwachtUnity/Assets/Scripts/Patrol.cs
@@ -36,8 +36,6 @@ public class Patrol : MonoBehaviour
     private void Update()
     {
 
-        CheckCrouch();
-
         if (PlayerInSightRange() || PlayerInAttackRange())
             Chase();
         else
